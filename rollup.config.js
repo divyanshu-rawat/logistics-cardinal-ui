089108eebcb5d1f0eca7527803eec862c006c9cd
Vinicius Souza <vinicius.souza@rocket-internet.com.br>
2018-05-13 14:02:32 +0200

    Fix rollup config and babel

--- rollup.config.js
+++ rollup.config.js
@@ -13,14 +13,16 @@ export default {
     {
       file: pkg.main,
       format: 'cjs',
+      sourcemap: true,
+      globals: { 'styled-components': 'styled' },
     },
     {
       file: pkg.module,
       format: 'es',
+      sourcemap: true,
+      globals: { 'styled-components': 'styled' },
     },
   ],
-  sourcemap: true,
-  globals: { 'styled-components': 'styled' },
   external: ['styled-components'],
   plugins: [
     external(),
